cca65878f3a80ee283b043a8b066d2613d1d9841
msy20020410 <dj8817117@gmail.com>
2025-09-04 09:09:48 +0800

    新增全局异常处理器

---
+++ business/src/main/java/com/xm/nls/business/controller/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.xm.nls.business.controller;
+
+import com.xm.nls.business.resp.CommonResp;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ */
+@Slf4j
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public CommonResp<Object> handler(Exception e) {
+        log.info("服务异常：", e);
+        CommonResp<Object> commonResp = new CommonResp<>();
+        commonResp.setSuccess(false);
+        commonResp.setMessage("服务异常,请联系管理员!");
+        return commonResp;
+    }
+}
