5fba68163a644bc76eedb85a81c57e113a2a7d27
msy20020410 <dj8817117@gmail.com>
2025-09-10 15:53:46 +0800

    引入ant-design-vue的图标包并全局组注册使用

--- web/src/main.js
+++ web/src/main.js
@@ -3,7 +3,13 @@ import './style.css'
 import App from './App.vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue';
 
 const app = createApp(App);
-
 app.use(Antd).mount('#app');
+
+// 全局使用图标
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
